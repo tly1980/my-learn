d3b61cce1b54aa34b6a8a1f72ec51c255e6ee99f
Tom Tang <tly1980@gmail.com>
2016-03-17 11:15:12 +1100

    adding the authentication

--- appengine/hello/hello.py
+++ appengine/hello/hello.py
@@ -1,10 +1,25 @@
 import webapp2
+from google.appengine.api import users
+
+class AuthPage(webapp2.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    if user:
+        greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+          (user.nickname(), users.create_logout_url('/')))
+    else:
+        greeting = ('<a href="%s">Sign in or register</a>.' %
+          users.create_login_url(self.request.path_qs))
+
+    self.response.out.write('<html><body>%s</body></html>' % greeting)
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/plain'
     self.response.write('Hello, "%s"!' % self.request.path_qs)
 
+
 app = webapp2.WSGIApplication([
-  ('/.*', MainPage),], 
-debug=True)
+  ('/a/.*', AuthPage), 
+  ('[^a].*', MainPage),
+  ], debug=True)
